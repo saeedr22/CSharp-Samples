3306c760f07c81b35978649f90226b8c19e7c3ec
Saeed Rezaei <saeed.rezaei1@gmail.com>
2022-03-12 16:28:49 +0330

    This And Constructor Samples

---
+++ ObjectOrientedProgrammin/Static/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using static System.DateTime;
+
+SavingAccount s1 = new SavingAccount(50);
+SavingAccount s2 = new SavingAccount(100);
+// Print the current interest rate.
+Console.WriteLine("Interest Rate is: {0}", SavingAccount.GetInterestRate());
+// Make new object, this does NOT 'reset' the interest rate.
+SavingAccount s3 = new SavingAccount(10000.75);
+Console.WriteLine("Interest Rate is: {0}", SavingAccount.GetInterestRate());
+//Output
+//Interest Rate is: 0.04
+//Interest Rate is: 0.04
+TimeUtilClass.PrintDate();
+TimeUtilClass.PrintTime();
+
+//A Simple Saving Account Class
+class SavingAccount
+{
+    //instance-Level Data
+    public double currBalance;
+    // A static point of data.
+    public static double currInterestRate = 0.04;
+    public SavingAccount(double balance)
+    {
+        currBalance = balance;
+    }
+
+    static SavingAccount()
+    {
+
+        Console.WriteLine("OK! Static constructor");
+    }
+
+    public static void SetInterestRate(double InterestRate)
+         => currInterestRate = InterestRate;
+
+    public static double GetInterestRate()
+            => currInterestRate;
+}
+
+// Static classes can only
+// contain static members!
+
+//notice this=> using static System.DateTime; => DateTime.Today.ToShortDateString() = Today.ToShortDateString()
+static class TimeUtilClass
+{
+    public static void PrintTime()
+    => Console.WriteLine(DateTime.Now.ToShortTimeString());
+    public static void PrintDate()
+    => Console.WriteLine(Today.ToShortDateString());
+}
+
---
+++ ObjectOrientedProgrammin/This/Program.cs
@@ -0,0 +1,38 @@
+﻿Motorcycle c = new Motorcycle(5);
+c.SetDriverName("Tiny");
+Console.WriteLine("Rider name is {0}", c.name);
+
+// the Role of the this Keyword
+class Motorcycle
+{
+    public int driverIntensity;
+    // New members to represent the name of the driver.
+    public string name = "";
+    public void SetDriverName(string name) => this.name = name;
+
+    //Chaining Constructor Calls Using this
+
+    public Motorcycle()
+    {
+         Console.WriteLine("In default ctor");
+    }
+    // This is the 'master' constructor that does all the real work.
+    public Motorcycle(int intensity, string name)
+    {
+        if (intensity > 10)
+        {
+            intensity = 10;
+        }
+        driverIntensity = intensity;
+        this.name = name;
+    }
+
+    public Motorcycle(string name) : this(0, name)
+    { 
+         Console.WriteLine("In ctor taking a string");
+    }
+    public Motorcycle(int intensity) : this(intensity, "")
+    { 
+         Console.WriteLine("In ctor taking an int");
+    }
+}
