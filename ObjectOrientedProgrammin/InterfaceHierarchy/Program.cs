b43c46dca161aa4f505ff0e92c64e864cd6c7e27
Saeed Rezaei <saeed.rezaei1@gmail.com>
2022-04-11 14:43:43 +0430

    Add InterfaceHierarchy Sample Project

---
+++ ObjectOrientedProgrammin/InterfaceHierarchy/Program.cs
@@ -0,0 +1,43 @@
+﻿// Call from object level.
+BitmapImage myBitmap = new BitmapImage();
+myBitmap.Draw();
+myBitmap.DrawInBoundingBox(10, 10, 100, 150);
+myBitmap.DrawUpsideDown();
+// Get IAdvancedDraw explicitly.
+if (myBitmap is IAdvancedDraw iAdvDraw)
+{
+    iAdvDraw.DrawUpsideDown();
+}
+// Get IDrawable explicitly.
+if (myBitmap is IDrawable iDrawable)
+{
+    // error
+    // iDrawable.DrawUpsideDown();
+    iDrawable.Draw();
+}
+public interface IDrawable
+{
+    void Draw();
+}
+
+public interface IAdvancedDraw : IDrawable
+{
+    void DrawInBoundingBox(int top, int left, int bottom, int right);
+    void DrawUpsideDown();
+}
+
+public class BitmapImage : IAdvancedDraw
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing...");
+    }
+    public void DrawInBoundingBox(int top, int left, int bottom, int right)
+    {
+        Console.WriteLine("Drawing in a box...");
+    }
+    public void DrawUpsideDown()
+    {
+        Console.WriteLine("Drawing upside down!");
+    }
+}
