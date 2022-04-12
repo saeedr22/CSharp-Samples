8f1b263acac389c71fa395d3cca9b89765bafdee
Saeed Rezaei <saeed.rezaei1@gmail.com>
2022-04-12 15:51:12 +0430

    Add InterfaceIComparable Sample Project

---
+++ ObjectOrientedProgrammin/InterfaceIComparable/Program.cs
@@ -0,0 +1,43 @@
+﻿var car = new List<Car>
+{
+    new Car() { Id = 2, PetName = "Car1", CurrentSpeed =100},
+    new Car() { Id = 1, PetName = "Car2", CurrentSpeed = 120 },
+    new Car() { Id = 3, PetName = "Car3", CurrentSpeed = 130 }
+};
+car.Sort();
+class Car : IComparable
+{
+    // Constant for maximum speed.
+    public const int MaxSpeed = 100;
+    // Car properties.
+    public int CurrentSpeed { get; set; } = 0;
+    public string PetName { get; set; } = "";
+    public int Id { get; set; } = 0;
+
+    // Constructors.
+    public Car() { }
+    public Car(string name, int speed, int id)
+    {
+        CurrentSpeed = speed;
+        PetName = name;
+        Id = id;
+    }
+
+    public int CompareTo(object? otherCar)
+    {
+        if (otherCar is Car car)
+        {
+            if (Id > car.Id)
+                return 1;
+            if (Id == car.Id)
+                return 0;
+            else return -1;
+        }
+        throw new ArgumentException("other car is not a Car");
+        // if (otherCar is Car temp)
+        // {
+        //     return this.Id.CompareTo(temp.Id);
+
+        // }
+    }
+}
