452066a3c7d636311ff3c5559ac0db084d78bb1a
Saeed Rezaei <saeed.rezaei1@gmail.com>
2022-04-13 16:27:14 +0430

    Add GarbageCollector Sample Projects

---
+++ ObjectOrientedProgrammin/GarbageCollector-sample01/Program.cs
@@ -0,0 +1,37 @@
+﻿// Print out estimated number of bytes on heap.
+Console.WriteLine("Estimated bytes on heap: {0}",
+ GC.GetTotalMemory(false));
+// MaxGeneration is zero based, so add 1 for display
+// purposes.
+Console.WriteLine("This OS has {0} object generations.\n",(GC.MaxGeneration + 1));
+Car refToMyCar = new Car("Zippy", 100);
+Console.WriteLine(refToMyCar.ToString());
+
+// Print out generation of refToMyCar object.
+Console.WriteLine("Generation of refToMyCar is: {0}",
+ GC.GetGeneration(refToMyCar));
+
+ // Only investigate generation 0 objects.
+GC.Collect(0);
+GC.WaitForPendingFinalizers();
+
+//As with any garbage collection, calling GC.Collect() promotes surviving generations. 
+Console.WriteLine("Generation of refToMyCar is: {0}",
+ GC.GetGeneration(refToMyCar));
+class Car 
+{
+    // Constant for maximum speed.
+    public const int MaxSpeed = 100;
+    // Car properties.
+    public int CurrentSpeed { get; set; } = 0;
+    public string PetName { get; set; } = "";
+
+    // Constructors.
+    public Car() { }
+    public Car(string name, int speed)
+    {
+        CurrentSpeed = speed;
+        PetName = name;
+    }
+
+}
---
+++ ObjectOrientedProgrammin/GarbageCollectorSimpleDispose/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("***** Fun with Dispose *****\n");
+// Create a disposable object and call Dispose()
+// to free any internal resources.
+MyResourceWrapper rw = new MyResourceWrapper();
+rw.Dispose();
+// Implementing IDisposable.
+class MyResourceWrapper : IDisposable
+{
+    // The object user should call this method
+    // when they finish with the object.
+    public void Dispose()
+    {
+        // Clean up unmanaged resources...
+        // Dispose other contained disposable objects...
+        // Just for a test.
+        Console.WriteLine("***** In Dispose! *****");
+    }
+}
