c26cb518658eed8b7d44edbc1325c5dc1d6959d7
Saeed Rezaei <saeed.rezaei1@gmail.com>
2022-04-14 13:00:10 +0430

    Add LazyObjectInstantiation Sample Project

---
+++ ObjectOrientedProgrammin/LazyObjectInstantiation/Program.cs
@@ -0,0 +1,37 @@
+﻿// This caller does not care about getting all songs,
+// but indirectly created 10,000 objects!
+MediaPlayer myPlayer = new MediaPlayer();
+myPlayer.Play();
+class Song
+{
+    public string Artist { get; set; } = "";
+    public string TrackName { get; set; } = "";
+    public double TrackLength { get; set; }
+}
+
+class AllTracks
+{
+    // Our media player can have a maximum
+    // of 10,000 songs.
+    private Song[] _allSongs = new Song[10000];
+    public AllTracks()
+    {
+        // Assume we fill up the array
+        // of Song objects here.
+        Console.WriteLine("Filling up the songs!");
+    }
+}
+
+class MediaPlayer
+{
+    // Assume these methods do something useful.
+    public void Play() { /* Play a song */ }
+    public void Pause() { /* Pause the song */ }
+    public void Stop() { /* Stop playback */ }
+    private Lazy<AllTracks> _allSongs = new Lazy<AllTracks>();
+    public Lazy<AllTracks> GetAllTracks()
+    {
+        // Return all of the songs.
+        return _allSongs;
+    }
+}
