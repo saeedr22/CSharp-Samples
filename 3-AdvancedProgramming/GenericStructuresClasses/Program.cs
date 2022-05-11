a21d8d96fe24f19eddb34554fa737ecf0e426fcf
Saeed Rezaei <saeed.rezaei1@gmail.com>
2022-05-12 01:08:31 +0430

    Add Generic New Samples

---
+++ 3-AdvancedProgramming/GenericStructuresClasses/Program.cs
@@ -0,0 +1,78 @@
+﻿Console.WriteLine("***** Fun with Generic Structures *****\n");
+// Point using ints.
+Point<int> p = new Point<int>(10, 10);
+Console.WriteLine("p.ToString()={0}", p.ToString());
+p.ResetPoint();
+Console.WriteLine("p.ToString()={0}", p.ToString());
+Console.WriteLine();
+// Point using double.
+Point<double> p2 = new Point<double>(5.4, 3.3);
+Console.WriteLine("p2.ToString()={0}", p2.ToString());
+p2.ResetPoint();
+Console.WriteLine("p2.ToString()={0}", p2.ToString());
+Console.WriteLine();
+// Point using strings.
+Point<string> p3 = new Point<string>("i", "3i");
+Console.WriteLine("p3.ToString()={0}", p3.ToString());
+p3.ResetPoint();
+Console.WriteLine("p3.ToString()={0}", p3.ToString());
+
+Point<string> p4 = default;
+Point<int> p5 = default;
+PatternMatching(p4);
+PatternMatching(p5);
+//Pattern Matching with Generics (New 7.1)
+static void PatternMatching<T>(Point<T> p)
+{
+    switch (p)
+    {
+        case Point<string> pString:
+            Console.WriteLine("Point is based on strings");
+            return;
+        case Point<int> pInt:
+            Console.WriteLine("Point is based on ints");
+            return;
+    }
+}
+struct Point<T>
+{
+    private T _xPos;
+    private T _yPos;
+    // Generic constructor.
+    public Point(T xVal, T yVal)
+    {
+        _xPos = xVal;
+        _yPos = yVal;
+    }
+
+    // Generic properties.
+    public T X
+    {
+        get => _xPos;
+        set => _xPos = value;
+    }
+    public T Y
+    {
+        get => _yPos;
+        set => _yPos = value;
+    }
+    public override string ToString() => $"[{_xPos}, {_yPos}]";
+
+    // Reset fields to the default value of the type parameter.
+    // The "default" keyword is overloaded in C#.
+    // When used with generics, it represents the default
+    // value of a type parameter.
+    public void ResetPoint()
+    {
+        _xPos = default(T);
+        _yPos = default(T);
+    }
+
+    // Default Literal Expressions (New 7.1)
+    public void ResetPointLiteral()
+    {
+        _xPos = default;
+        _yPos = default;
+    }
+}
+
---
+++ 3-AdvancedProgramming/ObservableCollection/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.AccessControl;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+ObservableCollection<Person> persons = new ObservableCollection<Person>
+{
+   new Person{Id=1,Name="Saeed",Family="Rezaei"},
+   new Person{Id=1,Name="Amin",Family="Rezaei"}
+};
+
+persons.CollectionChanged += Persons_Changd;
+
+// Now add a new item.
+persons.Add(new Person { Id = 1, Name = "Ahmad", Family = "Rezaei" });
+// Remove an item.
+persons.RemoveAt(0);
+
+void Persons_Changd(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+{
+    // What was the action that caused the event?
+    Console.WriteLine("Action for this event: {0}", e.Action);
+    // They removed something.
+    if (e.Action == NotifyCollectionChangedAction.Remove)
+    {
+        Console.WriteLine("Here are the OLD items:");
+        foreach (Person p in e.OldItems)
+        {
+            Console.WriteLine(p.ToString());
+        }
+        Console.WriteLine();
+    }
+    // They added something.
+    if (e.Action == NotifyCollectionChangedAction.Add)
+    {
+        // Now show the NEW items that were inserted.
+        Console.WriteLine("Here are the NEW items:");
+
+        foreach (Person p in e.NewItems)
+        {
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Family { get; set; } = "";
+
+    public override string ToString()
+    {
+        return Name + " " + Family;
+    }
+}
