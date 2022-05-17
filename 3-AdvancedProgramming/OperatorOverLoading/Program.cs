f33ce4cdc727bff825f11e349363346ce3e34d0e
Saeed Rezaei <saeed.rezaei1@gmail.com>
2022-05-17 16:05:57 +0430

    Add New Samples

---
+++ 3-AdvancedProgramming/OperatorOverLoading/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Car car1 = new Car("Benz");
+Car car2 = new Car("BMW");
+Car car3 = new Car("Toyota");
+car1++;
+Car car4 = car1 + car2 + car3;
+Console.WriteLine(car4.ToString());
+class Car : IComparable
+{
+    public string Name { get; set; }
+    public Car(string name)
+    {
+        Name = name;
+    }
+    public static Car operator +(Car car1, Car car2)
+        => new Car(car1.Name + " " + car2.Name);
+    public static Car operator ++(Car car1)
+        => new Car(car1.Name + "Plus");
+    public static bool operator ==(Car car1, Car car2)
+        => car1.Name.Equals(car2);
+    public static bool operator !=(Car car1, Car car2)
+        => !car1.Name.Equals(car2);
+    public static bool operator <(Car car1, Car car2)
+        => car1.CompareTo(car2) < 0;
+    public static bool operator >(Car car1, Car car2)
+        => car1.CompareTo(car2) > 0;
+    public static bool operator <=(Car car1, Car car2)
+        => car1.CompareTo(car2) <= 0;
+    public static bool operator >=(Car car1, Car car2)
+        => car1.CompareTo(car2) >= 0;
+
+    public override string ToString()
+    {
+        return Name.ToString();
+    }
+
+    public int CompareTo(Car? obj)
+    {
+        return this.Name.CompareTo(obj.Name);
+    }
+}
---
+++ 3-AdvancedProgramming/SimpleIndexer/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System.Collections;
+PersonCollection p = new PersonCollection();
+p[0] = new Person { Id = 1, Name = "Saeed", Family = "Rezaei" };
+Console.WriteLine(p[0].Id + " " + p[0].Name + " " + p[0].Family);
+class PersonCollection : IEnumerable
+{
+    private ArrayList persons = new ArrayList();
+    public Person this[int index]
+    {
+        get => (Person)persons[index];
+        set => persons.Insert(index, value);
+    }
+     public Person this[string name]
+    {
+        get => (Person)persons[name];
+        set => persons.Insert(name, value);
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return persons.GetEnumerator();
+    }
+}
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Family { get; set; }
+}
