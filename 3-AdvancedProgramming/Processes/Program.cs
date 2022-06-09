eeb23a3b152f90a600a1921c106bb7b7ac39037f
Saeed Rezaei <saeed.rezaei1@gmail.com>
2022-06-09 11:51:55 +0430

    add process Sample Project

---
+++ 3-AdvancedProgramming/Processes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+ListAllRunningProcesses();
+
+void ListAllRunningProcesses()
+{
+    // Get all the processes on the local machine, ordered by
+    // PID.
+    var runningProcs =
+    from proc
+    in Process.GetProcesses(".")
+    orderby proc.Id
+    select proc;
+    // Print out PID and name of each process.
+    foreach (var p in runningProcs)
+    {
+        string info = $"-> PID: {p.Id}\tName: {p.ProcessName}";
+        Console.WriteLine(info);
+    }
+    Console.WriteLine("************************************\n");
+}
